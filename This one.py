5b715fcf1f746d28356fd0c7605884ca6c282532
otomakii <111259885+otomakii@users.noreply.github.com>
2023-10-10 22:26:42 +1300

    I added a file thingy Substitute proper questions and category names.

--- This one.py
+++ This one.py
@@ -1,130 +1,155 @@
-# Import the tkinter library for GUI
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 
 
-# Create a class for the Quiz Application
 class QuizApp:
     def __init__(self, root):
-        # Initialize the main window
         self.root = root
-        self.root.title("Quiz")  # Set the window title
-
-        # Questions, options to choose from and the answers
-        self.questions = [
-            {
-                "question": "What is 1 + 1?",
-                "options": ["2", "4", "5", "6"],
-                "answer": "2"
-            },
-            {
-                "question": "What is 2 + 2?",
-                "options": ["3", "4", "5", "6"],
-                "answer": "4"
-            },
-            {
-                "question": "What is 3 + 3?",
-                "options": ["3", "4", "5", "6"],
-                "answer": "6"
-            },
-            {
-                "question": "What is 4 + 4?",
-                "options": ["3", "4", "5", "8"],
-                "answer": "8"
-            },
-            {
-                "question": "What is 5 + 5?",
-                "options": ["2", "3", "1", "10"],
-                "answer": "10"
-            },
-            {
-                "question": "What is 6 + 6?",
-                "options": ["2", "window", "1", "12"],
-                "answer": "12"
-            },
-            {
-                "question": "What is 7 + 7?",
-                "options": ["2", "window", "1", "14"],
-                "answer": "14"
-            },
-            {
-                "question": "What is 8 + 8?",
-                "options": ["2", "window", "1", "16"],
-                "answer": "16"
-            },
-            {
-                "question": "What is 9 + 9?",
-                "options": ["2", "window", "1", "18"],
-                "answer": "18"
-            },
-            {
-                "question": "What is 10 + 10?",
-                "options": ["2", "window", "1", "20"],
-                "answer": "20"
-            },
-        ]
-        # Initialize variables to keep track of the current question and score
+        self.root.title("Quiz")
+        self.categories = ["Category 1", "Category 2", "Category 3"]
+        self.selected_category = None
+        self.category_label = tk.Label(root, text="Select a category:", font=("Arial", 14))
+        self.category_buttons = []
+        self.question_frame = tk.Frame(root)
+        self.questions = {
+            "Category 1": [
+                {
+                    "question": "What is 1 + 1?",
+                    "options": ["2", "4", "5", "6"],
+                    "answer": "2"
+                },
+                {
+                    "question": "What is 2 + 2?",
+                    "options": ["3", "4", "5", "6"],
+                    "answer": "4"
+                },
+            ],
+            "Category 2": [
+                {
+                    "question": "What is 3 + 3?",
+                    "options": ["3", "4", "5", "6"],
+                    "answer": "6"
+                },
+                {
+                    "question": "What is 4 + 4?",
+                    "options": ["3", "4", "5", "8"],
+                    "answer": "8"
+                },
+            ],
+            "Category 3": [
+                {
+                    "question": "What is 5 + 5?",
+                    "options": ["2", "3", "1", "10"],
+                    "answer": "10"
+                },
+                {
+                    "question": "What is 6 + 6?",
+                    "options": ["2", "window", "1", "12"],
+                    "answer": "12"
+                },
+            ],
+        }
         self.current_question = 0
         self.score = 0
-        self.total_questions = len(self.questions)
+        self.total_questions = 0
 
-        # Initialize the question label
-        self.question_label = tk.Label(root, text="", font=("Arial", 14))
+        self.question_label = tk.Label(self.question_frame, text="", font=("Arial", 14))
+
+        self.option_vars = []
+        self.option_buttons = []
+        self.next_button = tk.Button(self.question_frame, text="Next", command=self.check_answer)
+        self.score_label = tk.Label(self.question_frame, text="", font=("Arial", 14))
+        self.retry_button = tk.Button(self.question_frame, text="Start again", command=self.retry_quiz)
+        self.question_logic()
+        self.save_button = tk.Button(self.question_frame, text="Save Results", command=self.save_to_file)
+        self.save_button.pack()
+
+    def question_logic(self):
+        self.category_label.pack(pady=20)
+        for category in self.categories:
+            button = tk.Button(root, text=category, command=lambda c=category: self.select_category(c))
+            button.pack()
+            self.category_buttons.append(button)
+        self.question_frame = tk.Frame(root)
+        self.question_label = tk.Label(self.question_frame, text="", font=("Arial", 14))
         self.question_label.pack(pady=20)
 
-        # Initialize radio buttons and their variables for answer choices
-        self.option_vars = []  # List to store answer choice variables
-        self.option_buttons = []  # List to store radio buttons
-        for i in range(10):
+        self.option_buttons = []
+        for i in range(4):
             var = tk.StringVar()
-            var.set("")  # Initialize the variable
+            var.set("")
             self.option_vars.append(var)
 
         for i in range(4):
             option_button = tk.Radiobutton(
-                root, text="", value="", font=("Arial", 12))
+                self.question_frame, text="", value="", font=("Arial", 12))
             option_button.pack()
             self.option_buttons.append(option_button)
 
-        # Initialize the "Next" button
-        self.next_button = tk.Button(root, text="Next", command=self.check_answer)
+        self.next_button = tk.Button(self.question_frame, text="Next", command=self.check_answer)
         self.next_button.pack()
 
-        # Initialize the score label
-        self.score_label = tk.Label(root, text="", font=("Arial", 14))
+        self.score_label = tk.Label(self.question_frame, text="", font=("Arial", 14))
         self.score_label.pack(pady=20)
 
-        # Initialize the "Retry" button (initially hidden)
-        self.retry_button = tk.Button(root, text="Start again", command=self.retry_quiz)
-        self.retry_button.pack_forget()  # Hide retry button initially
+        self.retry_button = tk.Button(self.question_frame, text="Start again", command=self.retry_quiz)
+        self.retry_button.pack_forget()
 
-        # Start the quiz by displaying the first question
+    def save_to_file(self):
+        # Get the content you want to save (replace this with your data)
+        results = """
+        Category: {}
+        Total Questions: {}
+        Correct Answers: {}
+
+        Thank you for playing the quiz!
+        """.format(self.selected_category, self.total_questions, self.score)
+
+        # Ask the user to select a file for saving
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+
+        # Check if the user canceled the file dialog
+        if not file_path:
+            return
+
+        try:
+            # Write the data to the selected file
+            with open(file_path, 'w') as file:
+                file.write(results)
+            messagebox.showinfo("File Saved", "Data saved successfully.")
+        except Exception as e:
+            messagebox.showerror("Error", "Error occurred while saving the file:\n{}".format(e))
+
+    def select_category(self, category):
+        self.selected_category = category
+        self.total_questions = len(self.questions[category])
+        self.current_question = 0
+        self.score = 0
+        self.category_label.pack_forget()
+        for button in self.category_buttons:
+            button.pack_forget()
+        self.question_frame.pack()
         self.update_question()
 
     def update_question(self):
-        # Display the current question and answer options
-        if self.current_question < self.total_questions:
-            question_data = self.questions[self.current_question]
-            self.question_label.config(text=question_data["question"])
-            options = question_data["options"]
-
-            # Create radio buttons dynamically based on the number of answer choices
-            num_options = len(options)
-            for i in range(num_options):
-                self.option_buttons[i].config(
-                    text=options[i], variable=self.option_vars[self.current_question], value=options[i])
-
-            # Hide any remaining radio buttons (if any)
-            for j in range(num_options, 4):
-                self.option_buttons[j].pack_forget()
-        else:
-            self.hide_question()  # Hide the question and options
-            self.show_score()  # Show the score
+        if self.selected_category is not None:  # Check if category is selected
+            if self.current_question < self.total_questions:
+                question_data = self.questions[self.selected_category][self.current_question]
+                self.question_label.config(text=question_data["question"])
+                options = question_data["options"]
+                num_options = len(options)
+                for i in range(num_options):
+                    self.option_buttons[i].config(
+                        text=options[i], variable=self.option_vars[self.current_question], value=options[i])
+                for j in range(num_options, 4):
+                    self.option_buttons[j].pack_forget()
+            else:
+                self.hide_question()
+                self.show_score()
 
     def check_answer(self):
-        # Check the selected answer and update the score
         selected_option = self.option_vars[self.current_question].get()
-        correct_answer = self.questions[self.current_question]["answer"]
+        correct_answer = self.questions[self.selected_category][self.current_question]["answer"]
 
         if not selected_option:
             messagebox.showerror("Error", "Please select an answer.")
@@ -136,45 +161,48 @@ class QuizApp:
         self.current_question += 1
 
         for var in self.option_vars:
-            var.set("")  # Clear the radio button selection
+            var.set("")
 
         if self.current_question < self.total_questions:
             self.update_question()
         else:
-            self.hide_question()  # Hide the question and options
-            self.show_score()  # Show the score
+            self.hide_question()
+            self.show_score()
 
     def hide_question(self):
-        # Hide the question label, radio buttons, and next button
         self.question_label.pack_forget()
         for button in self.option_buttons:
             button.pack_forget()
         self.next_button.pack_forget()
 
     def show_score(self):
-        # Show the score and the retry button
         score_text = f"You got {self.score}/{self.total_questions} correct answers"
         self.score_label.config(text=score_text)
-        self.retry_button.pack()  # Show retry button
-        self.retry_button.config(state=tk.NORMAL)  # Enable retry button
+        self.retry_button.pack()
+        self.retry_button.config(state=tk.NORMAL)
+
+    def show_category_selection(self):
+        self.selected_category = None
+        self.category_label.pack()
+        for button in self.category_buttons:
+            button.pack()
+        self.question_frame.pack_forget()
+        self.update_question()
 
     def retry_quiz(self):
-        # Reset the quiz and hide the retry button
         self.current_question = 0
         self.score = 0
         self.score_label.config(text="")
-        self.retry_button.pack_forget()  # Hide retry button when retrying
-        self.update_question()  # Show the first question and options again
-        self.question_label.pack()
-        for button in self.option_buttons:
-            button.pack()
-        self.next_button.pack()
+        self.retry_button.pack_forget()
+        self.question_frame.pack_forget()
+        self.update_question()
+        self.question_logic()
 
 
 if __name__ == "__main__":
-    root = tk.Tk()  # Create the main window
-    app = QuizApp(root)  # Initialize the QuizApp class
-    root.geometry('400x300')  # Set the window size
-    root.title("Quiz")  # Set the window title
-    root.resizable(False, False)  # Disable window resizing
-    root.mainloop()  # Start the main event loop
+    root = tk.Tk()
+    app = QuizApp(root)
+    root.geometry('400x260')
+    root.title("Quiz")
+    root.resizable(False, False)
+    root.mainloop()
